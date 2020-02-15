e5045aedabc9580774d0395bd2dabb99daa8394f
hiroto302 <i.am.a.hiroto@gmail.com>
2020-02-15 13:54:22 +0900

    MagicEffectの活用
    生成した魔法をキューブの子オブジェクトにしたテストサンプル

---
+++ Assets/Scripts/MagicController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicController : MonoBehaviour
+
+{
+    public GameObject magicPrefab;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            Instantiate(magicPrefab, gameObject.transform.position, Quaternion.identity,gameObject.transform);
+        }
+    }
+}

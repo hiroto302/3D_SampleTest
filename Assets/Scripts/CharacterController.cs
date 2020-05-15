c3e2326c9320af3e8e36fb77cfae10d9ca3fe6de
hiroto302 <i.am.a.hiroto@gmail.com>
2020-05-15 17:10:11 +0900

    一人称視点のPlayer操作機能の実装例

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    // // キャラクター操作に関して
+    // CharacterController characterController;
+    // // キャラクターの速度
+    // Vector3 velocity;
+    // // キャラクターの歩くスピード
+    // [SerializeField]
+    // private float walkSpeed = 2f;
+    // // キャラクターの走るスピード
+    // [SerializeField]
+    // private float runSpeed = 4f;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     characterController = GetComponent<CharacterController>();
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     // キャラクターの操作機能のみの記述
+    //     // キャラクターが接地しているかどうか
+    //     if(characterController.isGrounded)
+    //     {
+    //         velocity = Vector3.zero; // Vector3.zeroを入れて速度を0にする
+    //         var input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));  //横軸の入力,縦軸の入力
+    //         // input.magnitudeで入力（ベクトル）の長さを取得
+    //         if(input.magnitude > 0.1f) {
+    //             //transform.LookAt 引数で指定したベクトルの方向を向かせるメソッド
+    //             // input.normalizedで入力の単位ベクトル（長さが1のベクトルで方向を得る）を足すことで現在の位置に入力した方向を足して、その方向を向かせるようにします。
+    //             transform.LookAt(transform.position + input.normalized);
+    //         }
+    //     }
+    //     velocity.y += Physics.gravity.y * Time.deltaTime;
+    //     characterController.Move(velocity * Time.deltaTime);
+    // }
+    private CharacterController controller;
+
+    [SerializeField]
+    private float speed = 3.0f;
+    private float gravity = 9.8f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        CalculateMove();
+    }
+
+    void CalculateMove()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        Vector3 velocity = direction * speed;
+        velocity.y -= gravity;
+        velocity = transform.transform.TransformDirection(velocity);
+        // controller.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerControllerFPS.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerFPS : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+
+	void Start()
+	{
+		// 自分のRigidbodyを取ってくる
+		m_Rigidbody = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		// 十字キーで首を左右に回す
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f));
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.Rotate(new Vector3(0.0f, -1.0f, 0.0f));
+		}
+
+		// WASDで移動する
+		float x = 0.0f;
+		float z = 0.0f;
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			x += 1.0f;
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			x -= 1.0f;
+		}
+		if (Input.GetKey(KeyCode.W))
+		{
+			z += 1.0f;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			z -= 1.0f;
+		}
+
+        // rigidbody の速度を直接いじる
+		// m_Rigidbody.velocity = new Vector3(x, 0.0f, z);
+        // 上記の記述だとカメラの向きと移動方向が合わないので下記のように記述する
+        // Debug.Log(transform.forward + ": forward");
+        // Debug.Log(transform.right + ": right");
+        // Debug.Log(m_Rigidbody.velocity + ": velocity");
+
+        m_Rigidbody.velocity = z * transform.forward + x * transform.right;
+	}
+}
